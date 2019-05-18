0a3515e5bd85d2f107915b6717a0f20cea83889e
Bohodar <bodik264478@gmail.com>
2019-05-18 23:48:48 +0300

    Created footer, added adaptibility

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let keyUp = document.querySelector('.up-key');
 window.onscroll = upriseOfKeyUp;
 console.dir(keyUp);
 function upriseOfKeyUp() {
-      if (window.pageYOffset > 300) {
+      if (window.pageYOffset > 250) {
       keyUp.style.transform = 'translateY(0px)';
     } else { 
     	keyUp.style.transform = 'translateY(100px)';
